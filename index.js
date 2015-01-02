f8a550ffa56efbb45c55e3c802cbfc7a7c6ed428
guybedford <guybedford@gmail.com>
2015-01-02 17:01:11 +0200

    fix require

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (System._nodeRequire)
-  return module.exports = require('http');
+  return module.exports = System._nodeRequire('http');
 
 var http = module.exports;
 var EventEmitter = require('events').EventEmitter;
